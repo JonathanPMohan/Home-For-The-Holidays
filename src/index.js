b22b227b14b4c099a5761cf40dca1a261964197a
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-11-13 18:58:03 -0600

    updated navbar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 // import firebase from 'firebase/app';
 import './index.scss';
 import holidays from './images/holidays.png';
+import navbar from './components/Navbar/navbar';
 
 $('#holidays').attr('src', holidays);
+
+navbar();
