691aaa8638b210cd4ded268cb7b1e2e184b726bf
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-11-13 18:40:55 -0600

    Updated Setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import $ from 'jquery';
+import 'bootstrap';
+// import firebase from 'firebase/app';
+import './index.scss';
+import holidays from './images/holidays.png';
+
+$('#holidays').attr('src', holidays);
