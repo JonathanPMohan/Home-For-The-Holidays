c59664423767e98f2d16f84e37d6868c9c2a9146
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-11-20 20:05:56 -0600

    Updated Add Friend

--- src/components/AddEditFriends/addEditFriends.js
+++ src/components/AddEditFriends/addEditFriends.js
@@ -1,5 +1,7 @@
 import $ from 'jquery';
 import authHelpers from '../../helpers/authHelpers';
+import friendsData from '../../helpers/data/friendsData';
+import initializeFriendsPage from '../FriendsPage/friendsPage';
 
 const formBuilder = () => {
   const form = `
@@ -9,19 +11,19 @@ const formBuilder = () => {
   </div>
   <div class="form-group">
     <label for="form-friend-address">ADDRESS:</label>
-    <input type="text class="form-control" id="form-friend-address" placeholder="500 Interstate Blvd">
+    <input type="text" class="form-control" id="form-friend-address" placeholder="500 Interstate Blvd">
   </div>
   <div class="form-group">
     <label for="form-friend-email">EMAIL:</label>
-    <input type="text class="form-control" id="form-friend-email" placeholder="johnNotSmith@gmail.com">
+    <input type="text" class="form-control" id="form-friend-email" placeholder="johnNotSmith@gmail.com">
   </div>
   <div class="form-group">
     <label for="form-friend-phone">PHONE:</label>
-    <input type="text class="form-control" id="form-friend-phone" placeholder="555.555.5555">
+    <input type="text" class="form-control" id="form-friend-phone" placeholder="555.555.5555">
   </div>
   <div class="form-group">
     <label for="form-friend-relationship">RELATIONSHIP:</label>
-    <input type="text class="form-control" id="form-friend-relationship" placeholder="Arch Nemesis">
+    <input type="text" class="form-control" id="form-friend-relationship" placeholder="Arch Nemesis">
   </div>
   `;
 
@@ -38,6 +40,30 @@ const gettingFriendFromForm = () => {
     isAvoiding: false,
     uid: authHelpers.getCurrentUid(),
   };
-  console.log(friend);
+  return (friend);
 };
-export default { formBuilder, gettingFriendFromForm };
+
+const buildAddForm = () => {
+  let domString = '<h2>Add New Friend</h2>';
+  domString += formBuilder();
+  domString += '<button id="add-friend">Add Friend</button>';
+  $('#add-edit-friend').html(domString).show();
+  $('#friends').hide();
+};
+
+const addNewFriend = () => {
+  const newFriend = gettingFriendFromForm();
+  friendsData.addNewFriend(newFriend)
+    .then(() => {
+      $('#add-edit-friend').html('').hide();
+      $('#friends').show();
+      initializeFriendsPage();
+    })
+    .catch((error) => {
+      console.error('error', error);
+    });
+};
+
+$('body').on('click', '#add-friend', addNewFriend);
+
+export default buildAddForm;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import firebase from 'firebase/app';
 
@@ -11,12 +11,14 @@ import navbar from './components/Navbar/navbar';
 import loginButton from './components/Auth/auth';
 import authHelpers from './helpers/authHelpers';
 import friendsPage from './components/FriendsPage/friendsPage';
+import showAddForm from './components/AddEditFriends/addEditFriends';
 
 const initializeApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   navbar();
   authHelpers.checkLoginStatus(friendsPage);
   loginButton();
+  $('#show-friend-form').on('click', showAddForm);
 };
 
 initializeApp();
